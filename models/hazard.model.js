12291e55c8d52822edf3e6938294e3c0d8c9e144
Charlotte -Schroer <charlotte.schroer@student.ehb.be>
2025-12-30 14:47:42 +0100

    Update hazard.model.js
    Add validation + FK with Categories

--- models/hazard.model.js
+++ models/hazard.model.js
@@ -4,7 +4,13 @@ const HazardSchema = mongoose.Schema(
     {
         name: {
             type: String,
-            required: [true, "Please enter the hazard name"]
+            required: [true, "Please enter the hazard name"],
+            validate: {
+                validator: function (v) {
+                    return !/\d/.test(v);
+                },
+                message: "Name cannot contain numbers"
+            }
         },
 
         description: {
@@ -12,9 +18,21 @@ const HazardSchema = mongoose.Schema(
             required: [true, "Please enter the hazard description"]
         },
 
+        severity: {
+            type: Number,
+            required: [true, "Please enter severity (1-5)"],
+            min: [1, "Severity must be at least 1"],
+            max: [5, "Severity cannot exceed 5"]
+        },
+
+        category: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'Category',
+            required: [true, "Please select a category"]
+        }
     },
     {
-        Timestamps: true,
+        timestamps: true
     }
 );
 
